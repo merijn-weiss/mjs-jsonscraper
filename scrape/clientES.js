db412fffb7f0410d5924dc8d002fe8978b30971a
Merijn Weiss <Merijn.Weiss@kyndryl.com>
2022-09-11 12:11:15 +0200

    Initial commit

---
+++ scrape/clientES.js
@@ -0,0 +1,23 @@
+//* Module setup the ElasticClient */
+
+'use strict'
+require('dotenv').config();
+
+// Setup the Client
+const { Client } = require('@elastic/elasticsearch');
+
+
+
+const ElasticClient = new Client({
+  cloud: {
+    id: process.env.ES_CLOUDID
+  },
+  auth: {
+    username: process.env.ES_USERNAME,
+    password: process.env.ES_PASSWORD
+  }
+});
+
+module.exports = {
+    ElasticClient
+};
